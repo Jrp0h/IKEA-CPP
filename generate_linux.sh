4bc88d11e0a3081f2639ed5946be701e6afa7484
Marcus Nilsson <marcus.nilsson@genarp.com>
2020-10-22 10:10:18 +0200

    Changed from Premake to CMake

--- generate_linux.sh
+++ generate_linux.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-vendor/premake/premake5 gmake2 --os=linux
+# vendor/premake/premake5 gmake2 --os=linux
 # make CC="$HOME/.config/nvim/autoload/plugged/clang_complete/bin/cc_args.py gcc" CXX="$HOME/.config/nvim/autoload/plugged/clang_complete/bin/cc_args.py g++" -B
 
 # find . -name ".clang_complete" -type f | xargs cat | sort | uniq > .clang_complete
+
+mkdir build
+cd build
+
+cmake ..
+
+cp compile_commands.json ../compile_commands.json 
+
+make
